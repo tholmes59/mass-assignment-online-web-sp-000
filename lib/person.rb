ac17aae2f145ce5f94a2201f6beb05a605ed43b1
Tom Holmes <tholmes59@gmail.com>
2019-01-31 17:48:13 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,10 @@
 class Person
   
+  attr_accessor 
   
   def initialize(attributes)
-    attributes.each {|key, value| self.send ((#{key}=), vaule)}
+    attributes.each {|key, value| self.send (("#{key}="), vaule)}
+  end
   
   
 end
